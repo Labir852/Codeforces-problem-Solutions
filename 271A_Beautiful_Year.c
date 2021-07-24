b5410672c7b69711ef36124528c3ba96fc34aa94
Labir852 <tanvir.labir@gmail.com>
2021-07-24 20:54:47 +0600

    added 271A

--- 271A_Beautiful_Year.c
+++ 271A_Beautiful_Year.c
@@ -1,21 +1,48 @@
 #include<stdio.h>
+#include<stdlib.h>
+    int countDistinct(int n)
+{
+    int arr[10] = { 0 };
+    int count = 0;
+    while (n) {
+        int r = n % 10;
+        arr[r] = 1;
+        n /= 10;
+    }
+    for (int i = 0; i < 10; i++) {
+        if (arr[i])
+            count++;
+    }
+    return count;
+}
+int countDigit(int n)
+{
+    int c = 0;
+    while (n) {
+        int r = n % 10;
+        c++;
+        n /= 10;
+    }
+    return c;
+}
+int nextNumberDistinctDigit(int n)
+{
+    while (n < 9012){
+        int distinct_digits = countDistinct(n + 1);
+        int total_digits = countDigit(n + 1);
+ 
+        if (distinct_digits == total_digits) 
+            return n + 1;
+        else
+            n++;
+    }
+    return -1;
+}
 int main()
 {
-    int year,i=0,n,output,remaider[4],p,j;
+    int year,output;
     scanf("%d",&year);
-    p = year;
-    n = year;
-    while(n!=0)
-    {
-        remaider[i] = n % 10;
-        n/=10;
-        i++;
-    }
-    for ( i = 0; i < 4; i++)
-    {
-        for(j=i;j<n;j++)
-        
-    }
-    
+printf("%d",nextNumberDistinctDigit(year));
+
     return 0;
 }
