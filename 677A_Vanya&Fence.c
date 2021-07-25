b6d22df84a5a87fe264b0f5a4a9de48ac5d36762
Labir852 <tanvir.labir@gmail.com>
2021-07-25 13:36:01 +0600

    added tasks.json

---
+++ 677A_Vanya&Fence.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n, h,i,sum=0;
+    scanf("%d %d",&n, &h);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        if(h<a[i])
+            sum+=2;
+        else
+            sum+=1;
+    }
+    printf("%d",sum);
+    
+
+
+
+    return 0;
+}
